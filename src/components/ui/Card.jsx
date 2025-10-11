7dfc91ddc86c378770f19ffed65272acd9bd81b6
Ali-mohamed10 <messi01100665515@gmail.com>
2025-10-11 16:26:22 +0300

    added line-clamp to cards

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -25,10 +25,10 @@ export default function Card({
           />
         </Link>
         <div className="p-4 flex flex-col flex-1">
-          <h3 className="text-md md:text-lg font-bold mb-2">{title}</h3>
-          <p className="text-gray-600 mb-1">by {author}</p>
+          <h3 className="text-md md:text-lg font-bold mb-2 line-clamp-1">{title}</h3>
+          <p className="text-gray-600 mb-1 line-clamp-1">by {author}</p>
           <p className="text-main font-bold mb-2 text-2xl">${price}</p>
-          <p className="text-sm text-gray-500">{listName}</p>
+          <p className="text-sm text-gray-500 line-clamp-1">{listName}</p>
           <div className="mt-auto mx-auto">
             <CartButton uniqueId={uniqueId} />
           </div>
